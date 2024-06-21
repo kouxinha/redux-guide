f65fa4017bb7ffc544a4c7d15563293355d532df
kouxinha <gabrieldealmeidamoraes.com.br@gmail.com>
2024-06-21 09:53:53 -0400

    coloquei redux nos cards, e adicionei o redux-logger

---
+++ src/redux/cart/reducer.js
@@ -0,0 +1,17 @@
+import CartActionTypes from "./action-types";
+
+const initialState = {
+  products: [],
+  productsTotalPrice: 0
+};
+
+const cartReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CartActionTypes.ADD_PRODUCT:
+      return { ...state, products: [...initialState.products, action.payload] };
+    default:
+      return state;
+  }
+};
+
+export default cartReducer;
